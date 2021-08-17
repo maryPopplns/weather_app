f50ef35ac81a31eec405441a8311d2368eadcf92
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-17 15:59:22 -0700

    re-arranged app module layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,22 @@
 import 'regenerator-runtime/runtime';
 
 const APP = () => {
-  const FORM = document.getElementById('input_form');
-
   const FORM_SUBMIT_HANDLER = async (event) => {
     event.preventDefault();
     const API_KEY = process.env.weather_key;
     const USER_INPUT = document.getElementById('text_input').value;
-    const RESPONSE = await fetch(
-      `api.openweathermap.org/data/2.5/weather?q=${USER_INPUT}&appid=${API_KEY}`
-    );
-    console.log(RESPONSE);
+    try {
+      const RESPONSE = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${USER_INPUT}&appid=${API_KEY}`
+      );
+      console.log(RESPONSE);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  FORM.addEventListener('submit', FORM_SUBMIT_HANDLER);
+  const INPUT_FORM = document.getElementById('input_form');
+  INPUT_FORM.addEventListener('submit', FORM_SUBMIT_HANDLER);
 };
 
 export { APP };

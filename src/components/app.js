6cb3a03f33fcf9a4486b03f663890d20722b25f8
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-17 10:25:21 -0700

    made form submission handler its own function

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 const APP = () => {
   const FORM = document.getElementById('input_form');
 
-  FORM.addEventListener('submit', (event) => {
+  const FORM_SUBMIT_HANDLER = (event) => {
     event.preventDefault();
-    console.log(event);
-  });
+    const USER_INPUT = document.getElementById('text_input').value;
+    console.log(USER_INPUT);
+  };
+
+  FORM.addEventListener('submit', FORM_SUBMIT_HANDLER);
 };
 
 export { APP };

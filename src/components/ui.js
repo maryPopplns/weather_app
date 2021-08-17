b4f886ff166d1830a9359bd970e6cc8bbf542f24
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-17 09:48:46 -0700

    fixing git issues

--- src/components/ui.js
+++ src/components/ui.js
@@ -2,14 +2,11 @@ const UI = () => {
   const MAIN = document.createElement('main');
   const FORM = document.createElement('form');
   const INPUT = document.createElement('input');
-<<<<<<< HEAD
   const SUBMIT = document.createElement('input');
 
   INPUT.setAttribute('type', 'text');
   SUBMIT.setAttribute('type', 'submit');
   SUBMIT.id = 'submit_input';
-=======
->>>>>>> main
 
   document.body.append(MAIN);
   MAIN.append(FORM);

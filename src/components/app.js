4e7108620fbcb31eb4b52ab1e8fbb64f65ae9c66
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-17 11:29:30 -0700

    added dotenv packaage to handle api keys

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 const APP = () => {
   const FORM = document.getElementById('input_form');
 
-  const FORM_SUBMIT_HANDLER = (event) => {
+  const FORM_SUBMIT_HANDLER = async (event) => {
     event.preventDefault();
+    const API_KEY = process.env.weather_key;
     const USER_INPUT = document.getElementById('text_input').value;
-    console.log(USER_INPUT);
+    const RESPONSE = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${USER_INPUT}&appid=${API_KEY}`
+    );
+    console.log(RESPONSE);
   };
 
   FORM.addEventListener('submit', FORM_SUBMIT_HANDLER);
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    index: "./src/index.js",
+    index: './src/index.js',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Weather App",
+      title: 'Weather App',
     }),
+    new Dotenv(),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },

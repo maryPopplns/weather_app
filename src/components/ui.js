e821d42c1e627eaaf4f5359035170067443cf425
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-17 09:00:23 -0700

    added input submit button

--- src/components/ui.js
+++ src/components/ui.js
@@ -1,11 +1,17 @@
 const UI = () => {
-  const MAIN = document.createElement("main");
-  const FORM = document.createElement("form");
-  const INPUT = document.createElement("input");
+  const MAIN = document.createElement('main');
+  const FORM = document.createElement('form');
+  const INPUT = document.createElement('input');
+  const SUBMIT = document.createElement('input');
+
+  INPUT.setAttribute('type', 'text');
+  SUBMIT.setAttribute('type', 'submit');
+  SUBMIT.id = 'submit_input';
 
   document.body.append(MAIN);
   MAIN.append(FORM);
   FORM.append(INPUT);
+  FORM.append(SUBMIT);
 };
 
 export { UI };

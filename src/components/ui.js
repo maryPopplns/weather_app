da311938fd501da84563dc5be182ea963e8add78
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-17 09:46:51 -0700

    removed submit button

--- src/components/ui.js
+++ src/components/ui.js
@@ -2,19 +2,10 @@ const UI = () => {
   const MAIN = document.createElement('main');
   const FORM = document.createElement('form');
   const INPUT = document.createElement('input');
-<<<<<<< HEAD
-  const SUBMIT = document.createElement('input');
-
-  INPUT.setAttribute('type', 'text');
-  SUBMIT.setAttribute('type', 'submit');
-  SUBMIT.id = 'submit_input';
-=======
->>>>>>> main
 
   document.body.append(MAIN);
   MAIN.append(FORM);
   FORM.append(INPUT);
-  FORM.append(SUBMIT);
 };
 
 export { UI };

6adf5f91b6f8a98dc3d835bec0fc8cd5d344e652
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-18 12:18:00 -0700

    basic information showing correctly. only thing now is to style. will be continuing on to the next assignment

--- src/components/render_data.js
+++ src/components/render_data.js
@@ -5,19 +5,27 @@ const RENDER_DATA = (data) => {
   }
 
   const NAME = data.name;
-  // const WEATHER_DESCRIPTION = data.weather[0].description;
-  // const KELVIN_TEMP = data.main.temp;
+  const DESCRIPTION = data.weather[0].description;
+  const KELVIN_TEMP = data.main.temp;
   // const HUMIDITY = data.main.humidity;
 
   const DISPLAY_CONTAINER = document.createElement('div');
   const HEADING = document.createElement('h2');
+  const TEMPERATURE = document.createElement('div');
+  const WEATHER_DESCRIPTION = document.createElement('div');
 
   DISPLAY_CONTAINER.id = 'display_container';
   HEADING.id = NAME;
   HEADING.innerText = NAME;
+  TEMPERATURE.id = KELVIN_TEMP;
+  TEMPERATURE.innerText = KELVIN_TEMP;
+  WEATHER_DESCRIPTION.id = DESCRIPTION;
+  WEATHER_DESCRIPTION.innerText = DESCRIPTION;
 
   document.getElementsByTagName('main')[0].append(DISPLAY_CONTAINER);
   DISPLAY_CONTAINER.append(NAME);
+  DISPLAY_CONTAINER.append(TEMPERATURE);
+  DISPLAY_CONTAINER.append(WEATHER_DESCRIPTION);
 };
 
 export { RENDER_DATA };

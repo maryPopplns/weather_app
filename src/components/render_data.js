7ce176a85d7bd5d9521ec680d0ab5e17baae7ebb
Spencer Knight <spencer.knightusn@gmail.com>
2021-08-18 10:08:23 -0700

    changed variable naming

--- src/components/render_data.js
+++ src/components/render_data.js
@@ -4,16 +4,20 @@ const RENDER_DATA = (data) => {
     CURRENT_DATA.remove();
   }
 
-  // const NAME = data.name;
+  const NAME = data.name;
   // const WEATHER_DESCRIPTION = data.weather[0].description;
   // const KELVIN_TEMP = data.main.temp;
   // const HUMIDITY = data.main.humidity;
 
   const DISPLAY_CONTAINER = document.createElement('div');
+  const HEADING = document.createElement('h2');
 
   DISPLAY_CONTAINER.id = 'display_container';
+  HEADING.id = NAME;
+  HEADING.innerText = NAME;
 
   document.getElementsByTagName('main')[0].append(DISPLAY_CONTAINER);
+  DISPLAY_CONTAINER.append(NAME);
 };
 
 export { RENDER_DATA };
